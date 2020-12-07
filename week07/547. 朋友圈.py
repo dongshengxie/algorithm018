9c700b66489ce22634fd789713604382fc685f41
dongshengxie <964333261@qq.com>
2020-12-07 12:17:18 +0800

    week07 assignment

---
+++ week07/547. 朋友圈.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        f = {}
+        s = {}
+        count = len(M)
+
+        def find(x):
+            f.setdefault(x, x)
+            # 路径压缩
+            if x != f[x]:
+                f[x] = find(f[x])
+            return f[x]
+
+        def union(x, y):
+            nonlocal count
+            x_father, y_father = find(x), find(y)
+            if x_father == y_father: return
+            # 将小树的根节点连接到大叔的根节点
+            if s.setdefault(x_father, 1) < s.setdefault(y_father, 1):
+                f[x_father] = y_father
+                s[y_father] += s[x_father]
+            else:
+                f[y_father] = x_father
+                s[x_father] += s[y_father]
+            count -= 1
+
+        for i in range(len(M)):
+            for j in range(i + 1, len(M)):
+                if M[i][j] == 1:
+                    union(i, j)
+        return count
+
